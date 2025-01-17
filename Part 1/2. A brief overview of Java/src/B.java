063b4135cb8fc8670cf6cb210d0238e5adb1efd8
Alexandr Slelin <a.slelin.work@mail.ru>
2025-01-17 21:00:11 +0300

    Fix README.md & Create example java-files

---
+++ Part 1/2. A brief overview of Java/src/B.java
@@ -0,0 +1,8 @@
+public class B {
+    public static void main(String[] args) {
+        int num;
+        num = 100;
+        System.out.println("Значение num = " + num);
+        System.out.println("Значение num * 2 = " + (num * 2));
+    }
+}
---
+++ Part 1/2. A brief overview of Java/src/C.java
@@ -0,0 +1,14 @@
+public class C {
+    public static void main(String[] args) {
+        int x, y;
+        x = 10;
+        y = 20;
+
+        if(x < y) System.out.println("Значение x меньше y.");
+        x *= 2;
+        if(x == y) System.out.println("Теперь значение x равно y.");
+        x *= 2;
+        if(x > y) System.out.println("Теперь значение x больше y.");
+
+    }
+}
